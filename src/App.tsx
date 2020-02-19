55f0e7c5959859df1d3f6cef92ee7c96bf93e57a
Gupta <pulin.gupta@capgemini.com>
2020-02-19 07:41:06 +0200

    initial commit for ts project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LoginTab from "./components/login/login.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginTab />
+    </div>
+  );
+}
+
+export default App;
